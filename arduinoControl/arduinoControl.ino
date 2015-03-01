36d69e691ee9f821ab73eb2e89fff5637395fec6
John Farrell <johnfarrell@128-122-6-152.DYNAPOOL.NYU.EDU>
2015-02-28 20:23:06 -0500

    arduino update

---
+++ arduinoControl/arduinoControl.ino
@@ -0,0 +1,43 @@
+const int buttonPin = 2;
+#include <Bridge.h>
+#include <HttpClient.h>
+
+
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Starting bridge...\n");
+  Bridge.begin();
+  pinMode(buttonPin, INPUT);
+  delay(2000);
+  
+}
+
+
+void loop() {
+  
+  HttpClient client;  
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    Serial.println("high");
+    client.get("128.122.6.152:3000/readingButton");
+  } else {
+    Serial.println("low");
+    client.get("128.122.6.152:3000/dine2");
+  }
+//  //client.get("128.122.6.152:3000/table");
+  //client.get("128.122.6.152:3000/dine3");
+
+  
+  while (client.available()) {
+    char c = client.read();
+    Serial.print(c); 
+  }
+  Serial.flush();
+  delay(2000);
+}
+
+
